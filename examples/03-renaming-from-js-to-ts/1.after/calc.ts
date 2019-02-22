a8f50f4668b9205642826e7938a610f3363c283c
Erick Wendel <erick.workspace@gmail.com>
2019-02-21 18:44:56 -0800

    changes
    Signed-off-by: Erick Wendel <erick.workspace@gmail.com>

---
+++ examples/03-renaming-from-js-to-ts/1.after/calc.ts
@@ -0,0 +1,15 @@
+function sum(number1, number2) {
+    return number1 + number2
+}
+function sub(number1, number2) {
+    return number1 - number2
+}
+function mult(number1,number2) {
+    return number1 * number2
+}
+console.log(
+    `**** Math Class from JS****
+    1+1: ${sum(1,1)},
+    2-1: ${sub(2,1)},
+    10*2: ${mult(10, 2)}`
+)

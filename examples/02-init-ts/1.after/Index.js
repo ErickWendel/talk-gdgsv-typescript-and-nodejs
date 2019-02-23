0e8b7d639dacba89f49484f2fd2deb0bfca71f98
Erick Wendel <erick.workspace@gmail.com>
2019-02-22 18:21:19 -0800

    ts init
    Signed-off-by: Erick Wendel <erick.workspace@gmail.com>

---
+++ examples/02-init-ts/1.after/Index.js
@@ -0,0 +1,6 @@
+"use strict";
+function sum(number1, number2) {
+    return number1 + number2;
+}
+var result = sum(1, 2);
+console.log('result', result);
---
+++ examples/02-init-ts/1.after/Index.ts
@@ -0,0 +1,6 @@
+function sum (number1:number, number2:number): number {
+    return number1 + number2
+}
+
+const result: number = sum(1,2)
+console.log('result', result)
